2f789f4ebb4093122ed4d2e4e2276ad10f9f7159
Ahmed <ahmedmohamedaneng@gmail.com>
2023-02-13 20:17:02 +0200

    Add Marquee package
    package to display news line

--- lib/presentation/widgets/ads_widget.dart
+++ lib/presentation/widgets/ads_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:yamaiter/common/extensions/size_extensions.dart';
+import 'package:yamaiter/common/screen_utils/screen_util.dart';
+import 'package:yamaiter/presentation/themes/theme_color.dart';
 
 import '../../common/constants/app_utils.dart';
 import '../../common/constants/assets_constants.dart';
@@ -7,7 +10,6 @@ import '../../domain/entities/data/ad_entity.dart';
 import 'ads_list/ads_list_view.dart';
 
 class AdsWidget extends StatelessWidget {
-
   final List<AdEntity>? adsList;
 
   const AdsWidget({Key? key, this.adsList}) : super(key: key);
@@ -15,11 +17,44 @@ class AdsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: AppUtils.mainPagesHorizontalPadding.w,
-        vertical: AppUtils.mainPagesVerticalPadding.h,
+      padding: EdgeInsets.only(
+        right: AppUtils.mainPagesHorizontalPadding.w,
+        left: AppUtils.mainPagesHorizontalPadding.w,
+        top: AppUtils.mainPagesVerticalPadding.h,
       ),
-      child: AdsListViewWidget(),
+      child: Column(
+        children: [
+          AdsListViewWidget(),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: ScreenUtil.screenHeight * 0.05,
+            width: double.infinity,
+            color: AppColor.primaryDarkColor,
+            child: Marquee(
+              text:
+                  'خلافاَ للإعتقاد السائد فإن لوريم إيبسوم ليس نصاَ عشوائياً. لوريم إيبسوم(Lorem Ipsum) هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر ',
+              blankSpace: 100.0,
+              velocity: 50.0,
+              style: Theme.of(context)
+                  .textTheme
+                  .caption!
+                  .copyWith(color: AppColor.white),
+              // scrollAxis: Axis.horizontal,
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              // blankSpace: 20.0,
+              // velocity: 100.0,
+              // pauseAfterRound: Duration(seconds: 1),
+              // startPadding: 10.0,
+              // accelerationDuration: Duration(seconds: 1),
+              // accelerationCurve: Curves.linear,
+              // decelerationDuration: Duration(milliseconds: 500),
+              // decelerationCurve: Curves.easeOut,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
